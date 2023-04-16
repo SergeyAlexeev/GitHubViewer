2b775e3123fa920fe0a0e755c1b62f0b05ccceb0
Sergey Alexeev <s.alexeev@horo.school>
2023-04-16 15:02:43 +0200

    Initial Commit

---
+++ GitHubViewer/GitHubViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubViewerApp.swift
+//  GitHubViewer
+//
+//  Created by Алексеев Сергей Александрович on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
